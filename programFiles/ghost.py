dcd942ab36c959376abddba090256b002f01dc47
IzAKuLuS <izakulus@proton.me>
2026-08-30 19:29:58 -0700

    Added random commments to a file (for testing).

--- programFiles/ghost.py
+++ programFiles/ghost.py
@@ -8,3 +8,6 @@ class ghost(character):
     def __init__(self, name, health, skill, numAttacks):
         super().__init__(name, health, skill)
         self.numAttacks = numAttacks
+
+
+#sadl;kfkl;ajsd;ljkfasdfasdf
